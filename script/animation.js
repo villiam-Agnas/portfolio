42ff57a40bb8f0593ac6ad3339a2a6669cfa96e5
Villiam Agnas <villiamagnas@gmail.com>
2026-02-01 18:46:45 +0100

    finished animations and api

--- script/animation.js
+++ script/animation.js
@@ -1,7 +1,7 @@
-$(() => {
-    
+$(() => { 
     $('.first').fadeIn(600);
     $('.second').fadeIn(1200);
     $('.third').fadeIn(1800);
-    
+    $('.intro-text').fadeIn(1000)
+    $('.intro-short').fadeIn(3000)
 })    
